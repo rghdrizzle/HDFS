240d566c60cd71d8b82620404afebe1bf4e0697b
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2024-10-23 21:39:22 +0530

    code: refactored

--- main.go
+++ main.go
@@ -3,28 +3,43 @@ package main
 import (
 	"log"
 	"rghdrizzle/hdfs/p2p"
-	"time"
+	//"time"
 )
 
-func main(){
+func makeServer(listenAddr string,nodes ...string) *FileServer{ // ...string is used to recieve n number of arguments of type string ( n can be 0 as well )
+
 	tcpTransportOpts := p2p.TCPTransportOpts{
-		ListenAddr: ":3000",
+		ListenAddr: listenAddr,
 		Decoder: p2p.DefaultDecoder{},
 		HandShakeFunc: p2p.	NOPHandshakeFunc,
 	}
 	tcpTransport := p2p.NewTCPTransport(tcpTransportOpts)
 	fileServerOpts := FileServerOpts	{
-		StorageRoot: "3000_network",
+		StorageRoot: listenAddr+"_network",
+		PathTransformFromFunc: CASpathTransformFunc,
 		Transport: tcpTransport,
+		BootstrapNodes: nodes,
 	}
 	s := NewFileServer(fileServerOpts)
-	go func(){
-		time.Sleep(time.Second * 3)
-		s.Stop()
+	return s
+}
 
+func main(){
+	
+	s1 := makeServer(":3000","")
+	s2 := makeServer(":4000",":3000")
+	go func(){
+		log.Fatal(s1.Start())	
+		
 	}()
-	if err := s.Start();err!=nil{
-		log.Fatal(err)
-	}
+	s2.Start()
+	// go func(){
+	// 	time.Sleep(time.Second * 3)
+	// 	s.Stop()
+
+	// }()
+	// if err := s.Start();err!=nil{
+	// 	log.Fatal(err)
+	// }
 	
 }
--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"rghdrizzle/hdfs/p2p"
+	"sync"
 )
 
 type FileServerOpts struct {
@@ -15,6 +16,9 @@ type FileServerOpts struct {
 }
 type FileServer struct {
 	FileServerOpts 
+
+	peerLock sync.Mutex
+	peers map[string]p2p.Peer
 	store       *Store
 	quitch chan struct{}
 }
@@ -28,10 +32,14 @@ func NewFileServer(opts FileServerOpts) *FileServer {
 		FileServerOpts: opts,
 		store:          NewStore(storageOpts),
 		quitch: make(chan struct{}),
+		peers: make(map[string]p2p.Peer),
 	}
 }
+
 func (fs *FileServer) BootstrapNetwork() error{
+
 	for _, addr := range(fs.BootstrapNodes){
+		if len(addr)==0 {continue}
 		go func(addr string){
 			if err:= fs.Transport.Dial(addr); err!=nil{
 				log.Println("Dial error:",err)
@@ -52,6 +60,12 @@ func (fs *FileServer) Start() error {
 func (fs *FileServer) Stop(){
 	close(fs.quitch)
 }
+
+func (fs *FileServer) OnPeer(){
+		
+}
+
+
 func (fs *FileServer) loop(){
 	defer func(){
 		fmt.Println("Server stopped")
